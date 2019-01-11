d5792ab7fff300f2881869ea01936fc07d14ecce
NoahHoi365 <24922@ma-web.nl>
2019-01-11 11:19:37 +0100

    first commit

---
+++ Slasher/Assets/movement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    public float speed = 20f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        FaceMouse();
+
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+
+        transform.position = pos;
+
+    }
+
+    void FaceMouse()
+    {
+
+        Vector3 mouseposition = Input.mousePosition;
+        mouseposition = Camera.main.ScreenToWorldPoint(mouseposition);
+
+        Vector2 direction = new Vector2(mouseposition.x - transform.position.x, mouseposition.y - transform.position.y);
+        transform.up = direction;
+
+    }
+}
